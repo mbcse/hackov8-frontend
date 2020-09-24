275f69575741b456b28dd6da8aea2a7557f74fca
Priti Kumari <59758210+cypher-oo@users.noreply.github.com>
2020-09-25 01:25:40 +0530

    your Page

--- FRONTEND_HOMEPAGE/script.js
+++ FRONTEND_HOMEPAGE/script.js
@@ -145,5 +145,11 @@ const FOCUSABLE_SELECTORS = 'a[href], area[href], input:not([disabled]), select:
     searchBtn.addEventListener('click', searchmodal);
     searchc.addEventListener('click', searchc1);
 	
+   // Get the button, and when the user clicks on it, execute myFunction
+   document.getElementById("myBtn").onclick = function() {myFunction()};
 
+  /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+  function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+   }
     
